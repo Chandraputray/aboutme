7a389ef914d7262e5709e1d0e402aadc09d2bec3
Chandraputray <67107311+Chandraputray@users.noreply.github.com>
2020-06-18 07:14:59 -0700

    Menambah email dan nomor hp

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo 'nama' : Chandra putra Y;
 echo 'kelas' : XI RPL 1;
 echo 'asal sekolah' : Smk taruna bhakti;
-echo 'alamat' : kelurahan sukatani kecamatan tapos;
+echo 'no hp' : 08997702196;
+echo 'email' : mrslow714@gmail.com;
 ?>
